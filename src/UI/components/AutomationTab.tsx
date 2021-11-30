071bf6d911e1257bc1af31e5f954e85892c36639
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-11-30 19:51:26 +0530

    add sidebar corrections

--- src/UI/components/AutomationTab.tsx
+++ src/UI/components/AutomationTab.tsx
@@ -7,12 +7,25 @@ import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const { Header, Sider, Content } = Layout;
 
-const data = [
+const workflows = [
     'Message from x',
     'You were added to a group by x',
     'Status update from user x',
     'content',
     'content',
+];
+
+const conditions = [
+    'When',
+    'On schedule'
+    
+];
+
+const functions = [
+    'Set',
+    'Archive item',
+    'Run',
+   
 ];
 
 const AutomationTab = () => {
@@ -23,13 +36,14 @@ const AutomationTab = () => {
                     <span>
                         <ArrowLeftOutlined style={{ padding: 3, fontSize: 16, fontWeight: 600 }} />
                     </span>
-                    Workflows</Header>
+                    Workflows
+                    </Header>
                 <Layout>
                     <Sider width="25vw">
                         <Divider orientation="left">Default workflows</Divider>
                         <List
                             size="small"
-                            dataSource={data}
+                            dataSource={workflows}
                             renderItem={item =>
                                 <List.Item>{item}</List.Item>}
                         />
@@ -37,25 +51,47 @@ const AutomationTab = () => {
                     <Content className={"content-automation"}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis temporibus iure in, beatae totam quas sequi molestiae dolore excepturi delectus, veniam accusamus quisquam ratione mollitia fuga architecto illum quos eos?
                     </Content>
-                    <Sider width="20vw" style={{ display: "flex", justifyContent: "center" }}>
 
-                        <Divider orientation="center">
+                    <Sider width="22vw" >
+
+                        <Divider orientation="left">
                             <div className="chip">
                                 coming soon
                             </div>
 
                         </Divider>
-                        <div style={{ lineHeight: "normal", margin: 7, color: "#315C5C", fontSize: 13 }}>create and edit your workflow</div>
-                        <hr style={{ margin: 1 }} />
+                        <div style={{ lineHeight: "normal", margin: 8, color: "#315C5C", fontSize: 16 }}>create and edit your workflow</div>
+                        <hr style={{ marginInline: 10}} />
+                        
+                      <div className="divisionsider">
+                      <div className="siderTitle">Conditionals</div>
+                      
+                      <List
+                            size="small"
+                            className="List"
+                            dataSource={conditions}
+                            renderItem={item =>
+                                
+                                    <div className="rightlist" >
+                                        {item}
+                                    </div>}
+                        />
+                      </div>
+                       
+                        <div className="divisionsider">
+                        <div className="siderTitle">Functions</div>
+                       
                         <List
                             size="small"
-                            dataSource={data}
+                            className="List"
+                            dataSource={functions}
                             renderItem={item =>
-                                <List.Item >
-                                    <div style={{ margin: 1, border: "black" }}>
+                                
+                                    <div className="rightlist" >
                                         {item}
-                                    </div></List.Item>}
+                                    </div>}
                         />
+                        </div>
                     </Sider>
                 </Layout>
 
